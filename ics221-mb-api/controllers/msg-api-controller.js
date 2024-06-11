7e4e49525e962d1d052af144e1231dc92c7a8459
Noah Wiebe <wiebe.noahw@gmail.com>
2024-06-10 22:33:36 -0700

    Lab 4 - Task 5 - Complete

--- ics221-mb-api/controllers/msg-api-controller.js
+++ ics221-mb-api/controllers/msg-api-controller.js
@@ -1,15 +1,19 @@
-const [messages, setMessages] = useState([
+const messages = [
   { id: 0, name: "Bill", msgText: "Hi All!" },
   { id: 1, name: "Ann", msgText: "ICS 221 is fun!" },
   { id: 2, name: "Johnny", msgText: "I'm stranded!" },
   { id: 3, name: "Barb", msgText: "Hi" },
   { id: 4, name: "Frank", msgText: "Who's tired?" },
   { id: 5, name: "Sarah", msgText: "I heart React" },
-]);
+];
 
 // GET Request Handler
 const getAllMessages = (req, res) => {
-  res.status(200).json(messages);
+  try {
+    res.status(200).json(messages);
+  } catch {
+    res.status(400).send('Bad Request');
+  }
 };
 
 // POST Request Handler
