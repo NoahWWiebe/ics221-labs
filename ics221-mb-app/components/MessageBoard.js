b0844aafa54f9b75129feabfb308d65d373afbc4
Noah Wiebe <wiebe.noahw@gmail.com>
2024-06-25 17:36:15 -0700

    Passed api messages determined at build to the MessageBoard component

--- ics221-mb-app/components/MessageBoard.js
+++ ics221-mb-app/components/MessageBoard.js
@@ -2,15 +2,8 @@ import React, { useState } from 'react';
 import MessageTable from './MessageTable';
 import NewMessageForm from './NewMessageForm';
 
-const MessageBoard = () => {
-    const [messages, setMessages] = useState([
-        { id: 0, name: "Bill", msgText: "Hi All!" },
-        { id: 1, name: "Ann", msgText: "ICS 221 is fun!" },
-        { id: 2, name: "Johnny", msgText: "I'm stranded!" },
-        { id: 3, name: "Barb", msgText: "Hi" },
-        { id: 4, name: "Frank", msgText: "Who's tired?" },
-        { id: 5, name: "Sarah", msgText: "I heart React" },
-    ]);
+const MessageBoard = ({ jsonData }) => {
+    const [messages, setMessages] = useState(jsonData);
 
     const addNewMessage = (values) => {
         values.id = messages.length;
