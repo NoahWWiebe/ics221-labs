bf5b4236baea1c3f87895e2cf4b6c3a3fc712a79
Noah Wiebe <wiebe.noahw@gmail.com>
2024-06-25 17:46:17 -0700

    Changed addNewMessage to post the new message to the api instead of local state

--- ics221-mb-app/components/MessageBoard.js
+++ ics221-mb-app/components/MessageBoard.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
-import MessageTable from './MessageTable';
-import NewMessageForm from './NewMessageForm';
+import React, { useState } from "react";
+import MessageTable from "./MessageTable";
+import NewMessageForm from "./NewMessageForm";
+import axios from "axios";
 
 const MessageBoard = ({ jsonData }) => {
-    const [messages, setMessages] = useState(jsonData);
+  const [messages, setMessages] = useState(jsonData);
 
-    const addNewMessage = (values) => {
-        values.id = messages.length;
-        const updatedMessages = [values, ...messages];
+  const addNewMessage = async (values) => {
+    axios
+      .post("http://10.21.75.180:3004/v1/messages", values)
+      .then((response) => {
+        const updatedMessages = [response.data, ...messages];
         setMessages(updatedMessages);
-    };
+      })
+      .catch((error) => {
+        console.error("Error:", error.response.data);
+      });
+  };
 
-    return (
-        <>
-            <NewMessageForm addNewMessage={addNewMessage} />
-            <MessageTable messages={messages} />
-        </>
-    );
-}
+  return (
+    <>
+      <NewMessageForm addNewMessage={addNewMessage} />
+      <MessageTable messages={messages} />
+    </>
+  );
+};
 
 export default MessageBoard;
