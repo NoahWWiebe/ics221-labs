65d41b627dfb7dcb67acd20cbc4395d7ac973476
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-08 16:36:55 -0700

    added CORS

--- django-mb-api/mb_api/settings.py
+++ django-mb-api/mb_api/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party Apps
     'rest_framework',
+    'corsheaders',
     # Local App
     'msgs.apps.MsgsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'mb_api.urls'
 
