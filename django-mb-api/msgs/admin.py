79bbb2881cb5c0ad87d0c7009a05aa2ca651ee78
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-08 16:14:32 -0700

    Added Lab 6 to Main Repo

---
+++ django-mb-api/msgs/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Msg
+class MsgAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "msgText",
+    )
+        
+admin.site.register(Msg, MsgAdmin)
