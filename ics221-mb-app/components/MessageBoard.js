a822586d4617082e4f5e5980bc125945626dc63e
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-17 17:28:18 -0700

    Lab 7 - Task 9

--- ics221-mb-app/components/MessageBoard.js
+++ ics221-mb-app/components/MessageBoard.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 import MessageTable from "./MessageTable";
 import NewMessageForm from "./NewMessageForm";
-import axios from "axios";
+
+const ADD_MESSAGE = gql`
+  mutation AddMessage($message: MessageInput!) {
+    addMessage(message: $message) {
+      id
+      name
+      msgText
+    }
+  }
+`;
 
 const MessageBoard = ({ jsonData }) => {
   const [messages, setMessages] = useState(jsonData);
+  const [addMessage] = useMutation(ADD_MESSAGE);
 
   const addNewMessage = async (values) => {
-    axios
-      .post(`${process.env.NEXT_PUBLIC_HOST}/v1/messages`, values)
-      .then((response) => {
-        const updatedMessages = [response.data, ...messages];
-        setMessages(updatedMessages);
-      })
-      .catch((error) => {
-        console.error("Error:", error.response.data);
+    try {
+      const { data } = await addMessage({
+        variables: { message: values },
       });
+      setMessages([data.addMessage, ...messages]);
+    } catch (error) {
+      console.error("Error adding message:", error);
+      console.error("Error details:", error.networkError?.result || error.message);
+    }
   };
 
   return (
