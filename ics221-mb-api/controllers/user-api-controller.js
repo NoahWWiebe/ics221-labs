0ed1c95bf9b347c0d3f47239af3fbb5c036fdae3
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-24 15:11:29 -0700

    Stuck on Task 6 - Step 3 - Lab 8

--- ics221-mb-api/controllers/user-api-controller.js
+++ ics221-mb-api/controllers/user-api-controller.js
@@ -3,7 +3,34 @@ import mongoose from "mongoose";
 const userModel = mongoose.model("user");
 
 const registerNewUser = async (req, res) => {
-  res.status(200).send("Successful API New User POST Request");
+  //res.status(200).send("Successful API New User POST Request");
+  try {
+    if (await alreadyExists(req.body.email, req,body.username)) {
+        res.status(429).json({
+            error: "Already Exists",
+            message: "The user in the body of the Request is a duplicate."
+        });
+    } else {
+        let user = await userModel.create(req.body);
+        res.status(201).json({
+            message: `Succsesfully created user: ${user}`
+        });
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: "Bad Request",
+      message:
+        "The user in the body of the Request is either missing or malformed.",
+      details: err.errors || err.message,
+    });
+  }
 };
+
+// helper function to determine if email or username
+// already exists in the DB. Returns true or false.
+const alreadyExists = async (email, username) =>
+  await userModel.exists({
+    $or: [{ email: email }, { username: username }],
+  });
 
 export { registerNewUser };
