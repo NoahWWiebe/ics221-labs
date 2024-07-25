8cf73e12a3fec3d6a6cb5e7830a88e9c1b42384f
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-24 17:17:44 -0700

    protected POST req for the /messages endpoint

--- ics221-mb-api/routes/api-router.js
+++ ics221-mb-api/routes/api-router.js
@@ -4,9 +4,16 @@ import {
   addNewMessage,
 } from "../controllers/msg-api-controller.js";
 import { registerNewUser } from "../controllers/user-api-controller.js";
+import passport from "passport";
 
 const router = express.Router();
-router.route("/messages").get(getAllMessages).post(addNewMessage);
-router.route("/users").post(registerNewUser);
+
+router
+  .route("/messages")
+  .get(getAllMessages)
+  .post(passport.authenticate("basic", { session: false }), addNewMessage);
+
+router.route("/users")
+  .post(registerNewUser);
 
 export default router;
