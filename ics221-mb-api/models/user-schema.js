3ee6b39e820fd27b1d885d6d5a94bec8126a9810
Noah Wiebe <wiebe.noahw@gmail.com>
2024-07-24 17:09:42 -0700

    Added password hashing and salting

--- ics221-mb-api/models/user-schema.js
+++ ics221-mb-api/models/user-schema.js
@@ -1,34 +1,47 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import argon2 from "argon2";
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        match: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
-    },
-    username: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 15,
-        match: /^[a-zA-Z0-9_-]+$/
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 8,
-        maxLength: 64
-    }
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    match: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+  },
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 15,
+    match: /^[a-zA-Z0-9_-]+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 64,
+  },
 });
 
 userSchema.set("toJSON", {
-    versionKey: false,
-    virtuals: true,
-    transform: (doc, ret) => {
-      delete ret._id;
-    },
-  });
+  versionKey: false,
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+  },
+});
 
-export default mongoose.model('user', userSchema);
- 
+// Pre-Hook to Salt and Hash a password using argon2id
+userSchema.pre("save", async function () {
+  // hash and salt password
+  try {
+    const hash = await argon2.hash(this.password, {
+      type: argon2.argon2id,
+    });
+    this.password = hash;
+  } catch (err) {
+    console.log("Error in hashing password" + err);
+  }
+});
+
+export default mongoose.model("user", userSchema);
